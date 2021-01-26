4dc02bc4ce61eb7ff5f5ecf2e8a0e339e4adfb86
gksrlfw <gksrlfw@naver.com>
2021-01-26 23:27:23 +0900

    created project

---
+++ src/components/Searching.js
@@ -0,0 +1,14 @@
+export default class Searching {
+    constructor({ $target }) {
+        this.section = document.createElement('div');
+
+        this._render();
+        $target.appendChild(this.section);
+    }
+
+    _render() {
+        // input, 
+        
+
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+// https://velog.io/@hyeon930/%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EB%A6%AC%EC%8A%A4%EB%84%88-%EC%A4%84%EC%9D%B4%EA%B8%B0-Event-delegation
+
+import App from './App.js';
+
+const app = new App(document.querySelector('.app'));
